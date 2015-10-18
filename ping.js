dd361c8ad7214a0a0b60202344a824230460a8ee
Francis <dev@frpz.fr>
2015-10-19 01:28:37 +0200

    Add ping.range

--- ping.js
+++ ping.js
@@ -37,13 +37,33 @@ Ping.host = function (host) {
  * @returns {undefined}
  */
 Ping.range = function (range){
-  var i, ipv4Regex = /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
-	for(i = 0; i < range.length; i++){
-		if (ipv4Regex.test(range[i])){
-			var ping = Npm.require('jjg-ping');
-			ping.system.ping(range[i], function (latency, status) {
-				//Result is not needed
+	var i, ipv4Regex = /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+	var Future = Npm.require('fibers/future');
+	var ping   = Npm.require('jjg-ping');
+
+	var futures = _.map(range, function(ip) {
+		// Set up a future for the current job
+		var future = new Future();
+		// A callback so the job can signal completion
+		var onComplete = future.resolver();
+
+		/// Make async call
+		ping.system.ping(ip, function (latency, status) {
+			// Do whatever you need with the results here!
+			// Inform the future that we're done with it
+			onComplete(null, {
+				ip      : ip,
+				latency : latency,
+				online  : status,
+				status  : status ? 'online' : 'offline'
 			});
-		}
-	}
+		});
+		// Return the future
+		return future;
+	});
+
+	Future.wait(futures);
+	// and grab the results out.
+	return _.invoke(futures, 'get');
 };
+
